afc5f17b0fbab2495777b7158dc272e2c7736261
Jonathan Lee <jonathansmklee@yahoo.com>
2015-10-25 01:20:39 -0400

    init commit

--- hackingEDU/FirstViewController.swift
+++ hackingEDU/FirstViewController.swift
@@ -6,20 +6,124 @@
 //  Copyright © 2015 leej40. All rights reserved.
 //
 
+// features to possibly add
+// - variable pause between rows
+// - badass gui
+// - multiple subjects
+// - rearrange rows
+
+
+
 import UIKit
+import AVFoundation
 
-class FirstViewController: UIViewController {
+var itemArr = [String]()
+var delaySec = 0
 
+class FirstViewController: UIViewController, UITableViewDelegate {
+
+    @IBOutlet weak var tableList: UITableView!
+    
+    @IBAction func startEdit(sender: UIBarButtonItem) {
+        
+        tableList.editing = !tableList.editing
+        
+    }
+   
+    
+    
+    func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        
+        return true
+        
+    }
+    
+    func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+        
+        let itemToMove = itemArr[fromIndexPath.row]
+        itemArr.removeAtIndex(fromIndexPath.row)
+        itemArr.insert(itemToMove, atIndex: toIndexPath.row)
+    
+    }
+    
+    
+    let speechSynth = AVSpeechSynthesizer()
+    
+    // text to speech
+    @IBAction func startTest(sender: AnyObject) {
+        
+        if speechSynth.paused {
+            speechSynth.continueSpeaking()
+        }
+        else {
+            for each in itemArr {
+                let speech = AVSpeechUtterance(string: each)
+                speech.postUtteranceDelay = Double(delaySec)
+                speechSynth.speakUtterance(speech)
+            }
+        }
+    }
+    
+    @IBAction func pauseTest(sender: AnyObject) {
+        if speechSynth.speaking {
+            speechSynth.pauseSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+        }
+    }
+    
+    @IBAction func stopTest(sender: AnyObject) {
+        
+        speechSynth.stopSpeakingAtBoundary(AVSpeechBoundary.Immediate)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
 
+        if NSUserDefaults.standardUserDefaults().objectForKey("itemArr") != nil {
+            itemArr = NSUserDefaults.standardUserDefaults().objectForKey("itemArr")! as! [String]
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return itemArr.count
+        
+    }
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            itemArr.removeAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+            NSUserDefaults.standardUserDefaults().setObject(itemArr, forKey: "itemArr")
+        }
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = itemArr[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        tableList.reloadData()
+            
+        
+    }
 
-
+    
+        
 }
 
--- hackingEDU/SecondViewController.swift
+++ hackingEDU/SecondViewController.swift
@@ -8,11 +8,34 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
 
+
+class SecondViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var newItemField: UITextField!
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var delayLabel: UILabel!
+
+    @IBAction func delaySet(sender: UISlider) {
+        delaySec = Int(slider.value)
+        delayLabel.text = String(Int(slider.value*2)) + " sec"
+        
+    }
+    
+    @IBAction func AddNewPressed(sender: AnyObject) {
+   
+        
+        itemArr.append(newItemField.text!)
+        NSUserDefaults.standardUserDefaults().setObject(itemArr, forKey: "itemArr")
+        newItemField.text = ""
+        newItemField.resignFirstResponder()
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.newItemField.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +43,18 @@ class SecondViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+    
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+    
+        newItemField.resignFirstResponder()
+        AddNewPressed(self)
+        return true
+    }
 
 }
 
